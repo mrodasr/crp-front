28972ec1fca4537a8f0125c0d20cafb4091bd04f
Matias <mrodasr@outlook.es>
2023-01-02 03:21:32 -0300

    proxy

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,7 @@ const config = defineConfig({
         port: 8000,
         proxy: {
             '/api': {
-                target: 'http://localhost:5000',
+                target: 'http://127.0.0.1:5000',
                 changeOrigin: false
             }
         }
